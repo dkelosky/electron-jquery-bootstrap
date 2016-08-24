9d6895cbb9470f995aaed6190659b01e7b6cfba4
Dan Kelosky <Daniel.Kelosky@us.thalesgroup.com>
2016-08-24 12:20:32 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("@TEST");
+    
+    $('#min-test').click(function () {
+        console.log("clicked");
+    });    
+});
