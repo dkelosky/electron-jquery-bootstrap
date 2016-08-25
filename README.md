343ac3b4037db8cd4eb3689c55e3bf025b7d250e
dkelosky <dkelosky@gmail.com>
2016-08-24 22:20:07 -0400

    condense code

---
+++ README.md
@@ -0,0 +1,23 @@
+
+Prerequisites:
+===========
+nodejs is required 
+npm install -g electron (globally install - not installing locally in this project), isn't neccessary, but not covering other ways to get around this here.
+
+note proxy settings if download issues 
+
+git clone https://github.com/dkelosky/electron-jquery-bootstrap.git
+cd electron-jquery-bootstrap
+npm install 
+electron .
+
+After first setup:
+electron . 
+
+
+layout:
+css
+node_modules
+src + 
+index.html (main window)
+package
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 
+// main script file to set listers with jQuery and route click events 
+var setOS = require('./src/setOS');
+
+// wait for document to load 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("@TEST");
     
-    $('#min-test').click(function () {
-        console.log("clicked");
-    });    
+    initListeners(); // add listers
+    
 });
+
+function initListeners() {
+    console.log('initListeners called');
+    
+    $('#button-id').click(function () {  
+        handleOsDivClick(); // call this when button is clicked 
+    });    
+};
+
+function handleOsDivClick() {
+    console.log('handleOsDivClick called');
+    
+    setOS.setOsOnDiv('#os-div'); // pass div to dummy function 
+};
