610d9795e646cf1044d823ba179cc09b4393879a
Dan Kelosky <Daniel.Kelosky@us.thalesgroup.com>
2016-09-12 07:27:08 -0400

    remove local distribution of bootstrap & tether

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 
-// main script file to set listers with jQuery and route click events 
+// main script file to set listers with jQuery and route click events
+// require('tether'); // load all of Bootstrap's jQuery plugins
+require('bootstrap'); // load all of Bootstrap's jQuery plugins
 var setOS = require('./src/setOS');
 
-// wait for document to load 
+// wait for document to load
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     initListeners(); // add listers
-    
+
 });
 
 function initListeners() {
     console.log('initListeners called');
-    
-    $('#button-id').click(function () {  
-        handleOsDivClick(); // call this when button is clicked 
-    });    
+
+    $('#button-id').click(function () {
+        handleOsDivClick(); // call this when button is clicked
+    });
 };
 
 function handleOsDivClick() {
     console.log('handleOsDivClick called');
-    
-    setOS.setOsOnDiv('#os-div'); // pass div to dummy function 
+
+    setOS.setOsOnDiv('#os-div'); // pass div to dummy function
 };
