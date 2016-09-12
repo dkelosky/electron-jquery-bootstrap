fb541e09562e7407f0a7734bc975d8f4eb46c6af
Dan Kelosky <Daniel.Kelosky@us.thalesgroup.com>
2016-09-12 07:10:43 -0400

    using typescript

---
+++ src/js/setOSWithTS.js
@@ -0,0 +1,13 @@
+class Student {
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + "  " + middleInitial + " " + lastName;
+    }
+}
+function greeter(person) {
+    return "Hello ,     " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+console.log(user);
---
+++ src/setOSWithTS.ts
@@ -0,0 +1,24 @@
+// npm install -g typescript
+// tsc --init
+// tsc src/setOSWithTS.ts
+//
+// or, apm install atom-typescript
+//
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + "  " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello ,     " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
+console.log(user)
