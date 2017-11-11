5939013f8433cccaa2b1d3bf87757b7dc31d1956
kelda16 <daniel.kelosky@ca.com>
2017-11-10 20:40:01 -0500

    1st version compiling / running as typescript bassed

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,11 +1,20 @@
 // main script file to set listers with jQuery and route click events
 // require('bootstrap'); // load all of Bootstrap's jQuery plugins
-import { $ } from "bootstrap";
-import { OS } from "./util/OS";
-
+// import * as Popper from "popper.js";
+// import "bootstrap";
+// import Popper from "popper.js;
+// import $ from 'jquery'
+// import Popper from 'popper.js'
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import $ from "bootstrap"
+// import {$} from 'bootstrap';
+import { OS } from "./../util/OS";
+import * as $ from "jquery";
+// console.log("@TEST")
 // wait for document to load
 document.addEventListener("DOMContentLoaded", () => {
 
+    // let test = new Popper();
     initListeners(); // add listers
 
 });
